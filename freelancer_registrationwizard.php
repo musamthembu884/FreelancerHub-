12d96466a6ddfe84f8cc87d3f30f649160afa269
Lefa Mashele <Lefa Mashele>
2017-04-01 17:01:37 +0200

    added new files

---
+++ freelancer_registrationwizard.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en" class="coming-soon">
+<head>
+    <meta charset="utf-8">
+    <title>FreeLancer Hub</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="author" content="KaijuThemes">
+
+    <link type='text/css' href='http://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500' rel='stylesheet'>
+    <link type='text/css'  href="https://fonts.googleapis.com/icon?family=Material+Icons"  rel="stylesheet"> 
+    <link href="assets/plugins/progress-skylo/skylo.css" type="text/css" rel="stylesheet">                   <!-- Skylo -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href="assets/fonts/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+    <link href="assets/css/styles.css" type="text/css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries. Placeholdr.js enables the placeholder attribute -->
+    <!--[if lt IE 9]>
+        <link href="assets/css/ie8.css" type="text/css" rel="stylesheet">
+        <script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- The following CSS are included as plugins and can be removed if unused-->
+    
+    </head>
+
+    <body class="focused-form animated-content">
+        
+        
+ <div class="static-content-wrapper">
+                    <div class="static-content">
+                        <div class="page-content">
+                           
+                          
+                            <div class="container-fluid">
+                                
+
+
+
+	<div class="row">
+	<a href="#" class="login-logo"><img src="assets/img/logo-dark.png"></a>
+	    <div class="col-md-6 col-md-offset-3">
+
+			
+
+			<div class="panel panel-info" data-widget='{"draggable": "false"}'>
+				<div style="background-color:#42a5f5" class="panel-heading">
+					<h2 style="color:white">Registration Wizard</h2>
+				
+				</div>
+				<div class="panel-body">
+					
+					<form action="login.php" id="wizard" class="form-horizontal">
+						<fieldset title="Step 1">
+							<legend>Personal Address</legend>
+							<div class="form-group">
+											<label for="form-password" class="col-sm-2 control-label">Street</label>
+											<div class="col-sm-8 tabular-border">
+												<input type="text" class="form-control" id="form-password" placeholder="Street">
+											</div>
+										</div>
+										<div class="form-group">
+											<label for="form-confirmpass" class="col-sm-2 control-label">City</label>
+											<div class="col-sm-8 tabular-border">
+												<input type="text" class="form-control" id="form-confirmpass" placeholder="City">
+											</div>
+										</div>
+										<div class="form-group">
+											<label for="selector1" class="col-sm-2 control-label">Province</label>
+											<div class="col-sm-8 tabular-border"><select name="selector1" id="selector1" class="form-control">
+												
+												<option value="Western_Sahara">Select Province</option>
+												<option value="Yemen">Gauteng</option>
+												<option value="Zambia">Eastern Cape</option>
+												<option value="Zimbabwe">Mpumalanga</option>
+											</select></div>
+										</div>
+						</fieldset>
+						<fieldset title="Step 2">
+							<legend>Personal Description</legend>
+							<div class="form-group">
+											<label class="col-sm-2 control-label">Biography</label>
+											<div class="col-sm-8">
+												<textarea class="form-control fullscreen"></textarea>
+											</div>
+										</div>
+							<div class="form-group">
+											<label for="form-confirmpass" class="col-sm-2 control-label">Services/Work</label>
+											<div class="col-sm-8 tabular-border">
+												<input type="text" class="form-control" id="form-confirmpass" placeholder="Services/Work">
+											</div>
+										</div>
+										<div class="form-group">
+											<label for="radio" class="col-sm-2 control-label">Gender</label>
+											<div class="col-sm-8 tabular-border">
+												<div class="radio"><label><input type="radio" name="gender"> Male</label></div>
+												<div class="radio"><label><input type="radio" name="gender"> Female</label></div>
+											</div>
+										</div>
+						</fieldset>
+						<fieldset title="Step 3">
+							<legend>Terms and Conditions</legend>
+							<div class="form-group">
+								<label for="" class="col-md-3 control-label">Terms and Conditions</label>
+								<div class="col-md-9">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, nemo, atque consequuntur officiis asperiores consectetur porro labore commodi esse error quisquam nihil illum sunt facere inventore possimus autem ab voluptates quibusdam non voluptatum suscipit architecto. Illo, facilis, corporis, veritatis dolores minus quasi iure cupiditate quis autem ducimus nisi obcaecati tenetur sed ea excepturi pariatur consequatur enim labore officia mollitia. Rerum, voluptatem numquam molestiae recusandae iusto ipsum inventore unde accusantium labore delectus? Doloremque, fugit, sunt libero laboriosam cupiditate sed sequi nostrum saepe. Mollitia, alias, expedita accusantium porro error autem dolore veniam commodi nesciunt provident vitae neque. Nostrum, sed, molestias itaque provident inventore natus animi quasi laborum laboriosam facere ratione aperiam iusto. Non ducimus facere sunt doloribus? Asperiores, natus distinctio quis iure!</p>
+								</div>
+							</div>
+						</fieldset>
+						<input type="submit" class="finish btn-success btn" onclick="extras-profile.html" value="Submit" />
+					</form>
+				</div>
+			</div>
+
+
+	    </div>
+	</div>
+
+
+                            </div> <!-- .container-fluid -->
+                        </div> <!-- #page-content -->
+                    </div>
+                    <footer role="contentinfo">
+    <div class="clearfix">
+        <ul class="list-unstyled list-inline pull-left">
+            <li><h6 style="margin: 0;">&copy; 2016 KaijuThemes</h6></li>
+        </ul>
+    </div>
+</footer>
+
+                </div>
+                <div class="extrabar-underlay"></div>
+            </div>
+        </div>
+
+    
+    
+    <!-- Load site level scripts -->
+
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script> -->
+<script src="assets/plugins/form-validation/jquery.validate.min.js"></script>  					<!-- Validate Plugin -->
+<script src="assets/plugins/form-stepy/jquery.stepy.js"></script>  								<!-- Stepy Plugin -->
+
+<script src="assets/demo/demo-formwizard.js"></script>
+
+<script src="assets/js/jquery-1.10.2.min.js"></script> 							<!-- Load jQuery -->
+<script src="assets/js/jqueryui-1.10.3.min.js"></script> 							<!-- Load jQueryUI -->
+<script src="assets/js/bootstrap.min.js"></script> 								<!-- Load Bootstrap -->
+<script src="assets/js/enquire.min.js"></script> 									<!-- Load Enquire -->
+
+<script src="assets/plugins/velocityjs/velocity.min.js"></script>					<!-- Load Velocity for Animated Content -->
+<script src="assets/plugins/velocityjs/velocity.ui.min.js"></script>
+
+<script src="assets/plugins/progress-skylo/skylo.js"></script> 		<!-- Skylo -->
+
+<script src="assets/plugins/wijets/wijets.js"></script>     						<!-- Wijet -->
+
+<script src="assets/plugins/sparklines/jquery.sparklines.min.js"></script> 			 <!-- Sparkline -->
+
+<script src="assets/plugins/codeprettifier/prettify.js"></script> 				<!-- Code Prettifier  -->
+
+<script src="assets/plugins/bootstrap-tabdrop/js/bootstrap-tabdrop.js"></script>  <!-- Bootstrap Tabdrop -->
+
+<script src="assets/plugins/nanoScroller/js/jquery.nanoscroller.min.js"></script> <!-- nano scroller -->
+
+<script src="assets/plugins/dropdown.js/jquery.dropdown.js"></script> <!-- Fancy Dropdowns -->
+<script src="assets/plugins/bootstrap-material-design/js/material.min.js"></script> <!-- Bootstrap Material -->
+<script src="assets/plugins/bootstrap-material-design/js/ripples.min.js"></script> <!-- Bootstrap Material -->
+
+<script src="assets/js/application.js"></script>
+<script src="assets/demo/demo.js"></script>
+<script src="assets/demo/demo-switcher.js"></script>
+<script src="assets/plugins/form-validation/jquery.validate.min.js"></script>  					<!-- Validate Plugin -->
+<script src="assets/plugins/form-stepy/jquery.stepy.js"></script>  								<!-- Stepy Plugin -->
+
+<script src="assets/demo/demo-formwizard.js"></script>
+<!-- End loading site level scripts -->
+    <!-- Load page level scripts-->
+    
+    <!-- End loading page level scripts-->
+    </body>
+</html>
